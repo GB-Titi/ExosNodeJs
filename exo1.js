138b5d9dd95b2daa78ed1911db4b90265417a20d
GB-Titi <tristanmatteoli@gmail.com>
2021-02-23 15:30:31 +0100

    ajout exo 1

---
+++ exo1.js
@@ -0,0 +1,11 @@
+let number1 = "2";
+let number2 = "3";
+
+function addition(nb1, nb2){
+  let number1 = Number(nb1);
+  let number2 = Number(nb2);
+  let somme = number1 + number2;
+  console.log(somme.toString());
+}
+
+addition(number1, number2);
